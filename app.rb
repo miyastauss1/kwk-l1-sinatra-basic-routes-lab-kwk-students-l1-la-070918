2f9a04ae6a61f8189a3933f673a63a771c87b625
Miya Stauss <miyastauss1@gmail.com>
2018-07-17 21:27:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
-require_relative 'config/environment'
+require 'sinatra'
 
-class App < Sinatra::Base
+class App<Sinatra::Base
+  
+  get '/name' do
+    "Miya Maxine Stauss"
+  end
+  
+  get '/hometown' do
+    "Newport Beach, CA"
+  end
+  
+  get '/favorite-song' do
+    "King of my Heart by TS"
+  end
 end
+
+
